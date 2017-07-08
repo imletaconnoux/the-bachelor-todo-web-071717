0d3222d4796aa32ba791196382b1e345dd6ec9f8
Isabelle Letaconnoux <imletaconnoux@gmail.com>
2017-07-08 17:06:03 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,63 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
-end
+  data[season].each do |contestant|
+      contestant.each do |key, value|
+        if value == "Winner"
+         answer = contestant["name"]
+          return answer.split(' ').first
+     end
+   end
+ end
+ end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, cast|
+    cast.each do |contestant|
+      contestant.each do |key, value|
+      if value == occupation
+        return contestant["name"]
+      end
+    end
+  end
+end
 end
 
 def count_contestants_by_hometown(data, hometown)
+  counter = 0
+  data.each do |season, cast|
+    cast.each do |contestant|
+      contestant.each do |key, value|
+        if value == hometown
+          counter += 1
+        end
+      end
+    end
+  end
+  counter
   # code here
 end
 
 def get_occupation(data, hometown)
+  data.each do |season, cast|
+    cast.each do |contestant|
+      contestant.find do |key, value|
+        if value == hometown
+          return contestant["occupation"]
+        end
+      end
+    end
+  end
   # code here
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age_array = []
+  data[season].each do |contestant|
+    contestant.each do |key, value|
+      if key == "age"
+        age_array << contestant["age"].to_f
+      end
+    end
+  end
+  answer = (age_array.reduce(:+) / age_array.size.to_f).round
+  return answer
 end
